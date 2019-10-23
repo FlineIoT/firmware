a16d21be62476871ac7a51c175c7ec5fda8fe390
Adrien Chapelet <adrien3d@gmail.com>
2019-10-23 08:17:02 +0200

    Moved spi.h into bme280, and sigfox in app

--- fline/common/drivers/bme280.c
+++ fline/common/drivers/bme280.c
@@ -4,6 +4,12 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "nrf_drv_spi.h"
+#include "nrf_delay.h"
+#include "app_util_platform.h"
+#include "boards.h"
+#include "SEGGER_RTT.h"
+//#include "nrfx_spim.h"
 
 #include "bme280.h"
 
@@ -11,12 +17,91 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
+#define SPI_INSTANCE  0 /**< SPI instance index. */
+
 
 struct bme280_driver bme280; /* global instance */
 // Scheduler settings
 APP_TIMER_DEF(bme280_timer_id);                                             /** Creates timer id for our program **/
 #define SCHED_MAX_EVENT_DATA_SIZE       MAX(APP_TIMER_SCHED_EVT_SIZE, sizeof(nrf_drv_gpiote_pin_t))
 #define SCHED_QUEUE_SIZE                10
+
+/* PROTOTYPES *************************************************************************************/
+
+void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
+                       void *                    p_context);
+
+/* VARIABLES **************************************************************************************/
+static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
+volatile bool spi_xfer_done; /**< Semaphore to indicate that SPI instance completed the transfer. */
+static bool initDone = false;       /**< Flag to indicate if this module is already initilized */
+
+/* EXTERNAL FUNCTIONS *****************************************************************************/
+
+extern void spi_init(void)
+{
+    /* Conigure SPI Interface */
+    nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
+    spi_config.sck_pin = SPIM0_SCK_PIN;
+    spi_config.miso_pin = SPIM0_MISO_PIN;
+    spi_config.mosi_pin = SPIM0_MOSI_PIN;
+    spi_config.frequency = NRF_DRV_SPI_FREQ_4M;
+
+    /* Init chipselect for BME280 */
+    nrf_gpio_pin_dir_set(SPIM0_SS_HUMI_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+    nrf_gpio_cfg_output(SPIM0_SS_HUMI_PIN);
+    nrf_gpio_pin_set(SPIM0_SS_HUMI_PIN);
+
+    /* Init chipselect for LIS2DS12 */
+    nrf_gpio_pin_dir_set(SPIM0_SS_ACC_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+    nrf_gpio_cfg_output(SPIM0_SS_ACC_PIN);
+    nrf_gpio_pin_set(SPIM0_SS_ACC_PIN);
+
+    APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler, NULL));
+
+    spi_xfer_done = true;
+    initDone = true;
+}
+
+
+extern bool spi_isInitialized(void)
+{
+    return initDone;
+}
+
+extern SPI_Ret spi_transfer_bme280(uint8_t* const p_toWrite, uint8_t count, uint8_t* const p_toRead)
+{	
+	SPI_Ret retVal = SPI_RET_OK;
+	if ((NULL == p_toWrite) || (NULL == p_toRead))
+	{
+	    retVal = SPI_RET_ERROR;
+	}
+
+	
+    /* check if an other SPI transfer is running */
+    if ((true == spi_xfer_done) && (SPI_RET_OK == retVal))
+	{
+        spi_xfer_done = false;
+
+        nrf_gpio_pin_clear(SPIM0_SS_HUMI_PIN);
+        APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi, p_toWrite, count, p_toRead, count));
+        while (!spi_xfer_done)
+        {
+            //Requires initialized softdevice - TODO
+            uint32_t err_code = sd_app_evt_wait();
+            APP_ERROR_CHECK(err_code);
+            //__WFE(); 
+        }
+        nrf_gpio_pin_set(SPIM0_SS_HUMI_PIN);
+        retVal = SPI_RET_OK;
+	}
+	else
+	{
+	    retVal = SPI_RET_BUSY;
+	}
+
+    return retVal;
+}
 
 /* Prototypes */
 void timer_bme280_event_handler(void* p_context);
@@ -358,4 +443,18 @@ void timer_bme280_event_handler(void* p_context)
 {
     //NRF_LOG_INFO("BME\r\n");
     bme280_read_measurements(); //read previous data
+}
+
+/* INTERNAL FUNCTIONS *****************************************************************************/
+
+
+/**
+ * SPI user event handler
+ *
+ * Callback for Softdevice SPI Driver. Release blocking semaphore for SPI Transfer
+ */
+void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
+                       void *                    p_context)
+{
+    spi_xfer_done = true;
 }
--- fline/common/include/bme280.h
+++ fline/common/include/bme280.h
@@ -6,14 +6,49 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "app_error.h"
 #include "nrf.h"
-#include "spi.h"
 #include "nrf_drv_timer.h"
 #include "nrf_drv_gpiote.h"
 #include "bsp.h"
 #include "app_scheduler.h"
 #include "nordic_common.h"
 #include "app_timer.h"
+
+/** States of the module */
+typedef enum
+{
+    SPI_RET_OK = 0,   		    /**< Ok */
+    SPI_RET_BUSY = 1,			/**< Other SPI transfer in progress */
+    SPI_RET_ERROR = 2    	    /**< Not otherwise specified error */
+} SPI_Ret;
+
+/* PROTOTYPES *************************************************************************************/
+
+/**
+ * Initialize the SPI Wrapper
+ */
+extern void spi_init(void);
+
+/**
+ * Check if SPI Driver/Wrapper is initialized
+ *
+ * @return true Driver is initialized
+ * @return false Driver is not yet initialized
+ */
+extern bool spi_isInitialized(void);
+
+/**
+ * Send and receive bytes for bme280 environmental sensor
+ *
+ * @param[in] p_toWrite Data to transfer
+ * @param[out] p_toRead Receive buffer
+ * @param[in] count Size of p_toRead and p_toWrite
+ *
+ * @return SPI_RET_OK SPI transfer was successful
+ * @return SPI_RET_BUSY SPI is busy with other transfer, please try again
+ */
+extern SPI_Ret spi_transfer_bme280(uint8_t* const p_toWrite, uint8_t count, uint8_t* const p_toRead);
 
 struct comp_params {
   uint16_t dig_T1;
