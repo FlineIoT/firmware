6dd672006f07c43028645f808c9618f58459c03a
Cyril Fougeray <cyril@equisense.fr>
2019-10-27 15:04:34 -0300

    Fix paths for code formating script

--- utils/code/format-all.sh
+++ utils/code/format-all.sh
@@ -3,7 +3,6 @@ if [ "$#" -ne 1 ]; then
     echo "Please pass the root firmware directory as a parameter"
 fi
 
-find $1/motion/app \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
-find $1/motion/bootloader_secure_ble \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
-find $1/motion/common \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
-# find ../balios/dtm \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 ./formater.sh -i
+find $1/fline/app \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
+find $1/fline/bootloader \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
+find $1/fline/common \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) | xargs -n12 clang-format -i
